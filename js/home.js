491c7f2379d27e4d6ed581408ad914823adf01f3
benaor <benjamingirard25@gmail.com>
2020-08-21 01:38:24 +0200

    accordeon system in faq section

--- js/home.js
+++ js/home.js
@@ -2,20 +2,21 @@ new Vue({
     el: "#app",
     data : {
         faq: {
-        questions: [
-            {
-                sentence: 'Started several mistake joy say painful removed reached end',
-                answer: '1Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            },
-            {
-                sentence: 'Although moreover mistaken kindness me feelings do be marianne?',
-                answer: '2Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            },
-            {
-                sentence: 'By in no ecstatic wondered disposal my speaking?',
-                answer: '3Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            }
-        ]
-    }
+            currentQuestion: 0,
+            questions: [
+                {
+                    sentence: 'Started several mistake joy say painful removed reached end',
+                    answer: '1Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                },
+                {
+                    sentence: 'Although moreover mistaken kindness me feelings do be marianne?',
+                    answer: '2Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                },
+                {
+                    sentence: 'By in no ecstatic wondered disposal my speaking?',
+                    answer: '3Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
+                }
+            ]
+        }
     }
 })
