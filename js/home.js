2149f106e94bb6b06abc5986d39b7db4aaadb21c
benaor <benjamingirard25@gmail.com>
2020-08-21 16:24:15 +0200

    optimize portfolio section

--- js/home.js
+++ js/home.js
@@ -17,6 +17,51 @@ new Vue({
                     answer: '3Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
                 }
             ]
-        }
+        },
+        products: {
+            data: [
+                {
+                    name: 'Titre du produit 1',
+                    img: 'img/portfolio1.jpg',
+                    client: 'Envato 1',
+                    categories: ['Fashion', 'Design'],
+                    visit: 'envato.com',
+                    date: '01.09.2018'
+                },
+                {
+                    name: 'Titre du produit 2',
+                    img: 'img/portfolio2.jpg',
+                    client: 'Envato 2',
+                    categories: ['Fashion', 'Design'],
+                    visit: 'envato.com',
+                    date: '01.09.2018'
+                },
+                {
+                    name: 'Chaussure bleue',
+                    img: 'img/portfolio3.jpg',
+                    client: 'Envato 2',
+                    categories: ['Fashion', 'Design'],
+                    visit: 'envato.com',
+                    date: '01.09.2018'
+                },
+                {
+                    name: 'Titre du produit 4',
+                    img: 'img/portfolio4.jpg',
+                    client: 'Envato 2',
+                    categories: ['Fashion', 'Design'],
+                    visit: 'envato.com',
+                    date: '01.09.2018'
+                },
+                {
+                    name: 'Titre du produit 5',
+                    img: 'img/portfolio5.jpg',
+                    client: 'Envato 2',
+                    categories: ['Fashion', 'Design'],
+                    visit: 'envato.com',
+                    date: '01.09.2018'
+                }
+            ]
+        },
+    
     }
 })
